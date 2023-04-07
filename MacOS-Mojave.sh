fb739d1d5eec10b83df4e5c32b8538059c937226
DatTaiwanGuy <81352397+DatTaiwanGuy@users.noreply.github.com>
2023-04-06 16:19:55 -1200

    Update MacOS-Mojave.sh

--- MacOS-Mojave.sh
+++ MacOS-Mojave.sh
@@ -8,7 +8,7 @@ sudo apt install wget git qemu-system qemu-utils python3 python3-pip -y
 git clone https://github.com/foxlet/macOS-Simple-KVM.git
 cd macOS-Simple-KVM
 sudo python3  -m pip install click
-./jumpstart.sh --catalina
+./jumpstart.sh --ventura
 echo "Downloading Mac OS Catalina Disk..." 
 wget -q --show-progress --no-check-certificate -O MyDisk.qcow2 https://tinyurl.com/3s87mjea
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
